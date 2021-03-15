562b6e9071580a9ce3977fe011944811290b775c
Anmol Gupta <anmolgupta1311@gmail.com>
2021-03-15 18:41:10 +0530

    Fix heroku deployment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "p%p6+%oqzwg(ozz2&_pc159p69gu744*a8t88p(tgn!sdwz76e"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
