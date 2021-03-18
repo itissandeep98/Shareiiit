4a4e516a4bbf14d77191cd7f10e92dc8d4028b9d
Anmol Gupta <anmolgupta1311@gmail.com>
2021-03-19 03:23:16 +0530

    Add /logout/

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 # from .api import RegisterAPI, LoginAPI, UserAPI
 from . import views
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register("users", views.UserViewSet, basename="users")
@@ -12,6 +13,7 @@ urlpatterns = router.urls
 urlpatterns += [
     # path("users/", views.UserList.as_view()),
     # path("api-auth/", include("rest_framework.urls")),
-    path("login/", views.LoginView.as_view()),
+    path(r"login/", obtain_auth_token),
+    path(r"logout/", views.LogoutView.as_view())
     # path("users/<int:pk>/", views.UserDetail.as_view()),
 ]
