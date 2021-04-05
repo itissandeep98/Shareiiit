e732f385b1d1a0c7caa887abb1d15e0211688779
Anmol Gupta <anmolgupta1311@gmail.com>
2021-04-05 20:57:56 +0530

    Update on /profile/ endpoint

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -7,13 +7,14 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register("users", views.UserViewSet, basename="users")
-router.register("profile", views.UserDetailsViewSet, basename="profile")
+# router.register("profile", views.UserDetailsViewSet, basename="profile")
 urlpatterns = router.urls
 
 
 urlpatterns += [
     # path("users/", views.UserList.as_view()),
     # path("api-auth/", include("rest_framework.urls")),
+    path(r"profile/", views.UserProfileView.as_view()),
     path(r"login/", obtain_auth_token),
     path(r"logout/", views.LogoutView.as_view())
     # path("users/<int:pk>/", views.UserDetail.as_view()),
