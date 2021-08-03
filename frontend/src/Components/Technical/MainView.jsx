30155c28290f6da67fd3687054bfad7fd8a2289f
itissandeep98 <itissandeep98@gmail.com>
2021-08-03 12:55:28 +0530

    search and filter in skills

--- frontend/src/Components/Technical/MainView.jsx
+++ frontend/src/Components/Technical/MainView.jsx
@@ -8,15 +8,59 @@ import { fetchSkillPosts } from "../../Store/ActionCreators/skill";
 import { useState } from "react";
 import { connect, useDispatch } from "react-redux";
 
+const searchTest = function (varToSearch, jsonData) {
+  var ans = false;
+  for (var key in jsonData) {
+    if (ans) {
+      return true;
+    }
+    if (typeof jsonData[key] === "object") {
+      ans = ans || searchTest(varToSearch, jsonData[key]);
+    } else {
+      if (
+        typeof jsonData[key] === "string" &&
+        jsonData[key].toLowerCase().includes(varToSearch.toLowerCase())
+      ) {
+        return true;
+      }
+    }
+  }
+  return ans;
+};
+
 function MainView(props) {
   const { tags, modifyTags } = props;
   const [cards, setCards] = useState(props.skill.skills ?? []);
+  const [searchText, setSearchText] = useState("");
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchSkillPosts()).then((res) => {
       setCards(res);
     });
   }, [dispatch]);
+
+  useEffect(() => {
+    const allCards = props.skill.skills ?? [];
+    const temp = allCards.filter((card) => tags.includes(card.skill.name));
+    if (temp.length > 0) {
+      setCards(temp);
+    } else if (tags.length > 0) {
+      setCards([]);
+    } else {
+      setCards(props.skill.skills ?? []);
+    }
+  }, [tags]);
+
+  useEffect(() => {
+    const allCards = props.skill.skills ?? [];
+    const temp = allCards.filter((card) => searchTest(searchText, card));
+    if (temp.length > 0) {
+      setCards(temp);
+    } else {
+      setCards(props.skill.skills ?? []);
+    }
+  }, [searchText]);
+
   return (
     <Container className="bg-white p-3 rounded_lg">
       <Row>
@@ -24,6 +68,8 @@ function MainView(props) {
           <TextField
             label="Search Input"
             fullWidth
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
             InputProps={{
               endAdornment: (
                 <InputAdornment position="end">
@@ -53,11 +99,15 @@ function MainView(props) {
         </Col>
       </Row>
       <Row className="mt-3">
-        {cards?.map((card) => (
-          <Col md={4} className="mt-4">
-            <TechCard {...card} />
-          </Col>
-        ))}
+        {cards.length > 0 ? (
+          cards?.map((card) => (
+            <Col md={4} className="mt-4">
+              <TechCard {...card} />
+            </Col>
+          ))
+        ) : (
+          <p className="text-center text-muted w-100">No Results Found !!</p>
+        )}
       </Row>
     </Container>
   );
