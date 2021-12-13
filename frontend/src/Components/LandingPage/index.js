2697df62c181a198f9e707d1a2af72f33ff952ec
Sandeep <itissandeep98@gmail.com>
2021-12-13 15:52:34 +0530

    initial landing page

---
+++ frontend/src/Components/LandingPage/index.js
@@ -0,0 +1,48 @@
+import React, { useRef, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import ScrollReveal from "./utils/ScrollReveal";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
+import Hero from "./components/sections/Hero";
+import FeaturesTiles from "./components/sections/FeaturesTiles";
+import FeaturesSplit from "./components/sections/FeaturesSplit";
+import Testimonial from "./components/sections/Testimonial";
+import Cta from "./components/sections/Cta";
+import "./assets/scss/style.scss";
+
+const Index = () => {
+	const childRef = useRef();
+	let location = useLocation();
+
+	useEffect(() => {
+		document.body.classList.add("is-loaded");
+		childRef.current.init();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [location]);
+
+	return (
+		<ScrollReveal
+			ref={childRef}
+			children={() => (
+				<div id="landing">
+					<Header navPosition="right" className="reveal-from-bottom" />
+					<main className="site-content">
+						<Hero className="illustration-section-01" />
+						<FeaturesTiles />
+						<FeaturesSplit
+							invertMobile
+							topDivider
+							imageFill
+							className="illustration-section-02"
+						/>
+						<Testimonial topDivider />
+						<Cta split />
+					</main>
+					<Footer />
+				</div>
+			)}
+		/>
+	);
+};
+
+export default Index;
