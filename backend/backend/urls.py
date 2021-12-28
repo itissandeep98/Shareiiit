3960a8a17ab7b484d41e0d2f1b7a89b8c033777b
itissandeep98 <itissandeep98@gmail.com>
2021-12-29 00:21:18 +0530

    admin css fix

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("api/admin/", admin.site.urls),
     path("api/", include("accounts.urls")),
     path("api/", include("posts.urls")),
     path("api/", include("messaging.urls")),
